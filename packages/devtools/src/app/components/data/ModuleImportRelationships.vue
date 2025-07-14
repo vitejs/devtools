314cfbc1e16bcd67b0b09eaf67b181bbe949dff1
arlo <webfansplz@gmail.com>
2025-07-14 20:50:29 +0800

    chore: update

--- packages/devtools/src/app/components/data/ModuleImportRelationships.vue
+++ packages/devtools/src/app/components/data/ModuleImportRelationships.vue
@@ -21,12 +21,12 @@ type Link = HierarchyLink<Node> & {
 
 type LinkPoint = 'importer-start' | 'importer-end' | 'import-start' | 'import-end'
 
-const MAX_NODES = 5
+const MAX_LINKS = 20
 const SPACING = {
   width: 400,
   height: 50,
   padding: 4,
-  offsetX: 8,
+  marginX: 8,
   border: 1,
   margin: 8,
   dot: 16,
@@ -48,8 +48,22 @@ const importers = computed(() => {
   return props.module.importers?.map(x => modulesMap.value.get(x))
 })
 
-const maxNodes = computed(() => {
-  return Math.min(Math.max(props.module.importers?.length || 0, props.module.imports?.length || 0), MAX_NODES)
+const normalizedMaxLinks = computed(() => {
+  return Math.min(Math.max(props.module.importers?.length || 0, props.module.imports?.length || 0), MAX_LINKS)
+})
+
+const importersMaxLength = computed(() => Math.min(importers.value?.length || 0, MAX_LINKS))
+const importsMaxLength = computed(() => Math.min(props.module.imports?.length || 0, MAX_LINKS))
+const nodesHeight = computed(() => SPACING.height * normalizedMaxLinks.value + SPACING.padding * (normalizedMaxLinks.value + 1) + SPACING.border * 2)
+const importersVerticalOffset = computed(() => {
+  const diff = Math.max(0, importsMaxLength.value - importersMaxLength.value)
+  const offset = (diff * (SPACING.height + SPACING.padding)) / 2
+  return Math.min(offset, nodesHeight.value / 2)
+})
+const importsVerticalOffset = computed(() => {
+  const diff = Math.max(0, importersMaxLength.value - importsMaxLength.value)
+  const offset = (diff * (SPACING.height + SPACING.padding)) / 2
+  return Math.min(offset, nodesHeight.value / 2)
 })
 
 const createLinkHorizontal = linkHorizontal()
@@ -77,12 +91,10 @@ function getLinkColor(_link: Link) {
   return 'stroke-#8882'
 }
 
-const nodeContainerWidth = SPACING.width + SPACING.padding * 2 + SPACING.border * 2
-const nodeContainerHeight = computed(() => SPACING.height * maxNodes.value + SPACING.padding * (maxNodes.value + 1) + SPACING.border * 2)
-const dotNodeMargin = computed(() => `${nodeContainerHeight.value / 2 - SPACING.dot / 2}px ${SPACING.dotOffset}px 0  ${importers.value?.length ? SPACING.dotOffset : 0}px`)
-const linkStartX = computed(() => importers.value?.length ? nodeContainerWidth + SPACING.offsetX : SPACING.offsetX)
+const dotNodeMargin = computed(() => `${nodesHeight.value / 2 - SPACING.dot / 2}px ${SPACING.dotOffset}px 0  ${importers.value?.length ? SPACING.dotOffset : 0}px`)
+const linkStartX = computed(() => importers.value?.length ? SPACING.width + SPACING.marginX : SPACING.marginX)
 const dotStartX = computed(() => importers.value?.length ? linkStartX.value + SPACING.dotOffset : linkStartX.value)
-const dotStartY = computed(() => (SPACING.height * maxNodes.value + ((maxNodes.value + 1) * SPACING.padding)) / 2)
+const dotStartY = computed(() => (SPACING.height * normalizedMaxLinks.value + ((normalizedMaxLinks.value + 1) * SPACING.padding)) / 2)
 
 function calculateLinkX(type: LinkPoint) {
   switch (type) {
@@ -99,8 +111,9 @@ function calculateLinkX(type: LinkPoint) {
 function calculateLinkY(type: LinkPoint, i?: number) {
   switch (type) {
     case 'importer-start':
+      return ((SPACING.height + SPACING.padding) * i!) + (SPACING.height / 2 + SPACING.padding) + importersVerticalOffset.value
     case 'import-end':
-      return ((SPACING.height + SPACING.padding) * i!) + (SPACING.height / 2 + SPACING.padding)
+      return ((SPACING.height + SPACING.padding) * i!) + (SPACING.height / 2 + SPACING.padding) + importsVerticalOffset.value
     case 'importer-end':
     case 'import-start':
       return dotStartY.value
@@ -112,7 +125,7 @@ function generateLinks() {
 
   // importers (left -> current node)
   if (importers.value?.length) {
-    const _importersLinks = Array.from({ length: Math.min(importers.value.length, maxNodes.value) }, (_, i) => {
+    const _importersLinks = Array.from({ length: importersMaxLength.value }, (_, i) => {
       return {
         id: '',
         source: {
@@ -129,7 +142,7 @@ function generateLinks() {
   }
   // imports (current node -> right)
   if (props.module?.imports?.length) {
-    const _importsLinks = Array.from({ length: Math.min(props.module.imports.length, maxNodes.value) }, (_, i) => {
+    const _importsLinks = Array.from({ length: importsMaxLength.value }, (_, i) => {
       return {
         id: '',
         source: {
@@ -165,18 +178,16 @@ onMounted(() => {
       <!-- importers -->
       <div
         v-if="importers?.length"
-        border="~ base rounded" px2 py1
+        py1
         :style="{
-          overflow: 'auto',
-          width: `${nodeContainerWidth}px`,
-          height: `${nodeContainerHeight}px`,
-          padding: `${SPACING.padding}px`,
+          width: `${SPACING.width}px`,
+          marginTop: `${importersVerticalOffset}px`,
         }"
       >
         <template v-for="(importer, i) of importers" :key="importer.id">
           <DisplayModuleId
             :id="importer!.id"
-            hover="bg-active" block px2 p1 bg-glass
+            hover="bg-active" block px2 p1 bg-base
             z-graph-node
             border="~ base rounded"
             :link="true"
@@ -202,18 +213,16 @@ onMounted(() => {
       <!-- imports -->
       <div
         v-if="module.imports?.length"
-        border="~ base rounded" px2 py1
+        py1
         :style="{
-          overflow: 'auto',
-          width: `${nodeContainerWidth}px`,
-          height: `${nodeContainerHeight}px`,
-          padding: `${SPACING.padding}px`,
+          width: `${SPACING.width}px`,
+          marginTop: `${importsVerticalOffset}px`,
         }"
       >
         <template v-for="(_import, i) of module.imports" :key="_import.id">
           <DisplayModuleId
             :id="_import!.module_id"
-            hover="bg-active" block px2 p1 bg-glass
+            hover="bg-active" block px2 p1 bg-base
             z-graph-node
             border="~ base rounded"
             :link="true"
@@ -231,7 +240,12 @@ onMounted(() => {
     </div>
 
     <!-- links -->
-    <svg pointer-events-none absolute left-0 top-0 z-graph-link w-full h-180>
+    <svg
+      pointer-events-none absolute left-0 top-0 z-graph-link w-full
+      :style="{
+        height: `${nodesHeight}px`,
+      }"
+    >
       <g>
         <path
           v-for="link of links"
