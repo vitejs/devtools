ad4aa85cc5097ff139e93ddaffaa89db47d09da8
Anthony Fu <github@antfu.me>
2025-07-22 15:41:56 +0900

    chore: update deps

--- packages/devtools/src/app/components/plugins/FlatList.vue
+++ packages/devtools/src/app/components/plugins/FlatList.vue
@@ -9,7 +9,10 @@ defineProps<{
 <template>
   <div flex="~ col gap-2" p4>
     <template v-for="plugin of plugins" :key="plugin.id">
-      <div font-mono border="~ rounded base" px2 py1 text-sm hover="bg-active">
+      <div font-mono border="~ rounded base" px2 py1 text-sm hover="bg-active" flex="~ gap-4 items-center">
+        <div w-8 text-right text-xs op50>
+          #{{ plugin.plugin_id }}
+        </div>
         <DisplayPluginName :name="plugin.name" />
       </div>
     </template>
