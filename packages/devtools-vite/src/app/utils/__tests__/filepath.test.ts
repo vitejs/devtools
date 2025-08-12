7ceb126d1f8c35f81a837cd63d111c97a30d8c98
liangmiQwQ <mi_yyds@126.com>
2025-08-12 09:35:18 +0800

    test: add edge cases testing for #75

--- packages/devtools-vite/src/app/utils/__tests__/filepath.test.ts
+++ packages/devtools-vite/src/app/utils/__tests__/filepath.test.ts
@@ -62,19 +62,22 @@ describe('parseReadablePath', () => {
     expect(parseReadablePath('@vitejs%2Fdevtools', '/')).toEqual({ moduleName: '@vitejs/devtools', path: '@vitejs/devtools' })
   })
 
-  it('should return path with :', () => {
+  it('should return path with : unless Windows path', () => {
     expect(parseReadablePath('nuxt:index.mjs', '/')).toEqual({ moduleName: 'nuxt:index.mjs', path: 'nuxt:index.mjs' })
   })
 
   it('should return moduleName and subpath', () => {
     expect(parseReadablePath('/foo/node_modules/vite/dist/index.mjs', '/')).toEqual({ moduleName: 'vite', path: 'vite/dist/index.mjs' })
+    expect(parseReadablePath('C:\\foo\\node_modules\\vite\\dist\\index.mjs', 'C:\\')).toEqual({ moduleName: 'vite', path: 'vite/dist/index.mjs' })
   })
 
   it('should add ./ for no ./ items', () => {
     expect(parseReadablePath('/foo/index.mjs', '/foo')).toEqual({ path: './index.mjs' })
+    expect(parseReadablePath('C:\\foo\\index.mjs', 'C:\\foo')).toEqual({ path: './index.mjs' })
   })
 
   it('should add replace ./.nuxt to #build for .nuxt items', () => {
     expect(parseReadablePath('/foo/.nuxt/index.mjs', '/foo')).toEqual({ path: '#build/index.mjs' })
+    expect(parseReadablePath('C:\\foo\\.nuxt\\index.mjs', 'C:\\foo')).toEqual({ path: '#build/index.mjs' })
   })
 })
