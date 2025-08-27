046c97b4e488daa2132b90a1d0c7b78c93dc14e5
arlo <webfansplz@gmail.com>
2025-08-27 22:36:20 +0800

    feat: init

--- packages/devtools-vite/src/app/components/panel/SessionSelector.vue
+++ packages/devtools-vite/src/app/components/panel/SessionSelector.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { NuxtLink } from '#components'
 import { useRpc } from '#imports'
+
+defineProps<{
+  sessionMode: 'list' | 'compare'
+}>()
 
 const rpc = useRpc()
 const sessions = await rpc.value!['vite:rolldown:list-sessions']()
@@ -7,30 +12,37 @@ const sessions = await rpc.value!['vite:rolldown:list-sessions']()
 
 <template>
   <div flex="~ col gap-2">
-    <NuxtLink
-      v-for="session of sessions"
-      :key="session.id"
-      :to="`/session/${session.id}`"
-      hover="bg-active"
-      border="~ base rounded-md"
-      flex="~ col gap-1"
-      px4 py3
-    >
-      <div flex="~ gap-1 items-center" font-mono op50 text-sm>
-        <div i-ph-hash-duotone />
-        {{ session.id }}
-      </div>
-      <div font-mono font-sm>
-        {{ session.meta.cwd }}
-      </div>
-      <div v-if="session.meta.inputs[0]" flex="~ gap-1 items-center">
-        <DisplayModuleId :id="session.meta.inputs[0].filename" :cwd="session.meta.cwd" />
-        <DisplayBadge :text="session.meta.inputs[0].name || 'entry'" />
-        <span v-if="session.meta.inputs.length > 1" op50 text-xs border="~ base rounded-md" px1 font-mono>
-          +{{ session.meta.inputs.length - 1 }}
-        </span>
-      </div>
-      <DisplayTimestamp :timestamp="session.timestamp" pt2 text-sm op50 />
-    </NuxtLink>
+    <div v-for="session of sessions" :key="session.id" flex="~ row gap-2" relative>
+      <input
+        v-if="sessionMode === 'compare'"
+        class="absolute top-50% translate-y--50% left--5"
+        type="checkbox"
+        mr1
+      >
+      <component
+        :is="sessionMode === 'list' ? NuxtLink : 'div'"
+        :to="`/session/${session.id}`"
+        border="~ base rounded-md"
+        :class="sessionMode === 'list' ? 'hover:bg-active' : ''"
+        flex="~ col gap-1"
+        px4 py3
+      >
+        <div flex="~ gap-1 items-center" font-mono op50 text-sm>
+          <div i-ph-hash-duotone />
+          {{ session.id }}
+        </div>
+        <div font-mono font-sm>
+          {{ session.meta.cwd }}
+        </div>
+        <div v-if="session.meta.inputs[0]" flex="~ gap-1 items-center">
+          <DisplayModuleId :id="session.meta.inputs[0].filename" :cwd="session.meta.cwd" />
+          <DisplayBadge :text="session.meta.inputs[0].name || 'entry'" />
+          <span v-if="session.meta.inputs.length > 1" op50 text-xs border="~ base rounded-md" px1 font-mono>
+            +{{ session.meta.inputs.length - 1 }}
+          </span>
+        </div>
+        <DisplayTimestamp :timestamp="session.timestamp" pt2 text-sm op50 />
+      </component>
+    </div>
   </div>
 </template>
--- packages/devtools-vite/src/app/pages/index.vue
+++ packages/devtools-vite/src/app/pages/index.vue
@@ -1,9 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const sessionMode = ref<'list' | 'compare'>('list')
+
+const modeList = [
+  {
+    label: 'Session List',
+    icon: 'i-carbon-list',
+    value: 'list',
+  },
+  {
+    label: 'Session Compare',
+    icon: 'i-carbon-compare',
+    value: 'compare',
+  },
+] as const
+</script>
+
 <template>
-  <div p4 flex="~ col gap-4" items-center justify-center>
+  <div p4 flex="~ col gap-4" items-center justify-center relative>
     <VisualLogoBanner />
-    <p op50>
-      Select a build session to get started:
+    <p flex="~ row justify-around" w20 h8 border="~ base rounded-8" of-hidden absolute top-5 right-5>
+      <button v-for="mode in modeList" :key="mode.value" :title="mode.label" flex-1 op50 cursor-pointer flex="~ items-center justify-center" :class="{ 'bg-active text-white op100!': sessionMode === mode.value }" hover="bg-active text-white op100!" @click="sessionMode = mode.value">
+        <span :class="mode.icon" class="text-sm" />
+      </button>
     </p>
-    <PanelSessionSelector />
+    <p op50>
+      {{ sessionMode === 'list' ? 'Select a build session to get started:' : 'Select 2 sessions to compare:' }}
+    </p>
+    <PanelSessionSelector :session-mode="sessionMode" />
   </div>
 </template>
