d0dd4b59f84487b5fdc31f681af9473fcc6a001b
Anthony Fu <github@antfu.me>
2025-09-30 15:37:12 +0900

    chore: deps

--- packages/devtools/src/node/server.ts
+++ packages/devtools/src/node/server.ts
@@ -1,11 +1,6 @@
-import type { Buffer } from 'node:buffer'
 import type { CreateWsServerOptions } from './ws'
-import { readFile, stat } from 'node:fs/promises'
 import { createServer } from 'node:http'
-import { createApp, eventHandler, serveStatic, toNodeListener } from 'h3'
-import { lookup } from 'mrmime'
-import { join } from 'pathe'
-import { distDir } from '../dirs'
+import { createApp, eventHandler, toNodeListener } from 'h3'
 import { createWsServer } from './ws'
 
 export async function createDevToolsMiddleware(options: CreateWsServerOptions) {
@@ -13,36 +8,9 @@ export async function createDevToolsMiddleware(options: CreateWsServerOptions) {
 
   const { rpc, getMetadata } = await createWsServer(options)
 
-  const fileMap = new Map<string, Promise<string | Buffer<ArrayBufferLike> | undefined>>()
-  const readCachedFile = (id: string) => {
-    if (!fileMap.has(id))
-      fileMap.set(id, readFile(id).catch(() => undefined))
-    return fileMap.get(id)
-  }
-
   app.use('/api/connection.json', eventHandler(async (event) => {
     event.node.res.setHeader('Content-Type', 'application/json')
     return event.node.res.end(JSON.stringify(await getMetadata()))
-  }))
-
-  app.use('/', eventHandler(async (event) => {
-    const result = await serveStatic(event, {
-      fallthrough: true,
-      getContents: id => readCachedFile(join(distDir, id)),
-      getMeta: async (id) => {
-        const stats = await stat(join(distDir, id)).catch(() => {})
-        if (!stats || !stats.isFile())
-          return
-        return {
-          type: lookup(id),
-          size: stats.size,
-          mtime: stats.mtimeMs,
-        }
-      },
-    })
-
-    if (result === false)
-      return readCachedFile(join(distDir, 'index.html'))
   }))
 
   return {
