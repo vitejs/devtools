5da81037cbed2970206c2f8d65d35d4507ea25ff
pyyupsk <pyyupsk@proton.me>
2025-10-06 11:34:29 +0700

    refactor: remove error wrapping, rely on birpc's onError handler

--- packages/kit/src/utils/rpc.ts
+++ packages/kit/src/utils/rpc.ts
@@ -1,7 +1,4 @@
 import type { DevToolsNodeContext, RpcFunctionDefinition, RpcFunctionType } from '../types'
-import Debug from 'debug'
-
-const debug = Debug('vite:devtools:rpc')
 
 export function defineRpcFunction<
   NAME extends string,
@@ -23,39 +20,18 @@ export async function getRpcHandler<
   definition: RpcFunctionDefinition<NAME, TYPE, ARGS, RETURN>,
   context: DevToolsNodeContext,
 ): Promise<(...args: ARGS) => RETURN> {
-  let handler: (...args: ARGS) => RETURN
-
   if (definition.handler) {
-    handler = definition.handler
+    return definition.handler
   }
-  else if (definition.__resolved?.handler) {
-    handler = definition.__resolved.handler
+  if (definition.__resolved?.handler) {
+    return definition.__resolved.handler
   }
-  else {
-    definition.__promise ??= Promise.resolve(definition.setup(context))
-      .then((r) => {
-        definition.__resolved = r
-        definition.__promise = undefined
-        return r
-      })
-    const result = definition.__resolved ??= await definition.__promise
-    handler = result.handler
-  }
-
-  return ((...args: ARGS) => {
-    try {
-      const result = handler(...args)
-      if (result instanceof Promise) {
-        return result.catch((error) => {
-          debug(`RPC call "${definition.name}" failed:`, error)
-          throw error
-        }) as RETURN
-      }
-      return result
-    }
-    catch (error) {
-      debug(`RPC call "${definition.name}" failed:`, error)
-      throw error
-    }
-  }) as (...args: ARGS) => RETURN
+  definition.__promise ??= Promise.resolve(definition.setup(context))
+    .then((r) => {
+      definition.__resolved = r
+      definition.__promise = undefined
+      return r
+    })
+  const result = definition.__resolved ??= await definition.__promise
+  return result.handler
 }
