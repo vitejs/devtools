22b5b018dd1d3d18dad0a9194398f314d00ea754
arlo <webfansplz@gmail.com>
2026-02-10 14:41:32 +0800

    feat(core): export devtools config

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-export * from './node/config'
 export { createDevToolsContext } from './node/context'
 export { DevTools } from './node/plugins'
 export { createDevToolsMiddleware } from './node/server'
