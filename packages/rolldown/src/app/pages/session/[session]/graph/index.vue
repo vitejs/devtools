7deb769d2629bbfa99e3c39918746c028e77cf2e
Arash Ari Sheyda <hi@arashsheyda.me>
2026-03-17 19:32:39 -0600

    fix: update graph layout

--- packages/rolldown/src/app/pages/session/[session]/graph/index.vue
+++ packages/rolldown/src/app/pages/session/[session]/graph/index.vue
@@ -134,8 +134,8 @@ function toggleDisplay(type: ClientSettings['moduleGraphViewType']) {
 </script>
 
 <template>
-  <div relative max-h-screen of-hidden>
-    <div absolute left-4 top-4 z-panel-nav>
+  <div relative max-h-screen>
+    <div sticky left-4 top-4 right-4 z-panel-nav p-4>
       <DataSearchPanel v-model="searchValue" :rules="searchFilterTypes">
         <template v-if="pathSelectorVisible" #search>
           <DataPathSelector :session="session" :data="searched" import-id-key="module_id" :search-keys="['id']" @select="selectPathNodes" @close="togglePathSelector(false)">
@@ -170,7 +170,7 @@ function toggleDisplay(type: ClientSettings['moduleGraphViewType']) {
             </button>
           </div>
         </template>
-        <div flex="~ gap-2 items-center" p2 border="t base">
+        <div flex="~ wrap gap-2 items-center" p2 border="t base">
           <span op50 pl2 text-sm>View as</span>
           <button
             v-for="viewType of moduleViewTypes"
@@ -185,43 +185,35 @@ function toggleDisplay(type: ClientSettings['moduleGraphViewType']) {
         </div>
       </DataSearchPanel>
     </div>
-    <template v-if="settings.moduleGraphViewType === 'list'">
-      <div of-auto h-screen pt-45>
-        <ModulesFlatList
-          :session="session"
-          :modules="searched"
-        />
-        <div
-          absolute bottom-4 py-1 px-2 bg-glass left="1/2" translate-x="-1/2" border="~ base rounded-full" text="center xs"
-        >
-          <span op50>{{ searched.length }} of {{ session.modulesList.length }}</span>
-        </div>
-      </div>
-    </template>
-    <template v-else-if="settings.moduleGraphViewType === 'detailed-list'">
-      <div of-auto h-screen pt-45>
-        <ModulesDetailedList
-          :session="session"
-          :modules="searched"
-        />
-        <div
-          absolute bottom-4 py-1 px-2 bg-glass left="1/2" translate-x="-1/2" border="~ base rounded-full" text="center xs"
-        >
-          <span op50>{{ searched.length }} of {{ session.modulesList.length }}</span>
-        </div>
-      </div>
-    </template>
-    <template v-else-if="settings.moduleGraphViewType === 'graph'">
-      <ModulesGraph
-        :session="session"
-        :modules="normalizedGraph"
-      />
-    </template>
-    <template v-else>
-      <ModulesFolder
-        :session="session"
-        :modules="searched"
-      />
-    </template>
+    <ModulesFlatList
+      v-if="settings.moduleGraphViewType === 'list'"
+      :session="session"
+      :modules="searched"
+    />
+
+    <ModulesDetailedList
+      v-else-if="settings.moduleGraphViewType === 'detailed-list'"
+      :session="session"
+      :modules="searched"
+    />
+
+    <ModulesGraph
+      v-else-if="settings.moduleGraphViewType === 'graph'"
+      :session="session"
+      :modules="normalizedGraph"
+    />
+
+    <ModulesFolder
+      v-else
+      :session="session"
+      :modules="searched"
+    />
+
+    <div
+      v-if="settings.moduleGraphViewType === 'list' || settings.moduleGraphViewType === 'detailed-list'"
+      fixed bottom-4 py-1 px-2 bg-glass left="1/2" translate-x="-1/2" border="~ base rounded-full" text="center xs"
+    >
+      <span op50>{{ searched.length }} of {{ session.modulesList.length }}</span>
+    </div>
   </div>
 </template>
