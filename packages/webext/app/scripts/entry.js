4934fd18083fa1c735af696dfeb5b90308d99e2a
arlo <webfansplz@gmail.com>
2026-03-29 16:11:39 +0800

    feat: inject script

--- packages/webext/app/scripts/entry.js
+++ packages/webext/app/scripts/entry.js
@@ -1,6 +1,5 @@
 (async () => {
   // eslint-disable-next-line no-undef
   const script = chrome.runtime.getURL('dist/content-scripts.mjs')
-  console.log('scripts', script)
   await import(script)
 })()
