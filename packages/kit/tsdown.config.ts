cbee84caf8254930cd96dfcc5cec0b0714e172da
arlo <webfansplz@gmail.com>
2026-06-26 09:14:18 +0800

    build(kit): use node platform for node entry

--- packages/kit/tsdown.config.ts
+++ packages/kit/tsdown.config.ts
@@ -1,27 +1,34 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  entry: {
-    'index': 'src/index.ts',
-    'constants': 'src/constants.ts',
-    'utils/events': 'src/utils/events.ts',
-    'utils/nanoid': 'src/utils/nanoid.ts',
-    'utils/human-id': 'src/utils/human-id.ts',
-    'utils/when': 'src/utils/when.ts',
-    'utils/shared-state': 'src/utils/shared-state.ts',
-    'client': 'src/client/index.ts',
-    'node/index': 'src/node/index.ts',
-  },
+const common = {
   exports: true,
-  clean: true,
   tsconfig: '../../tsconfig.base.json',
   dts: true,
-  platform: 'neutral',
-  deps: {
-    onlyBundle: [
-      'human-id',
-      'immer',
-      'ua-parser-modern',
-    ],
+}
+
+export default defineConfig([
+  {
+    ...common,
+    entry: {
+      'index': 'src/index.ts',
+      'constants': 'src/constants.ts',
+      'utils/events': 'src/utils/events.ts',
+      'utils/nanoid': 'src/utils/nanoid.ts',
+      'utils/human-id': 'src/utils/human-id.ts',
+      'utils/when': 'src/utils/when.ts',
+      'utils/shared-state': 'src/utils/shared-state.ts',
+      'client': 'src/client/index.ts',
+    },
+    clean: true,
+    platform: 'neutral',
   },
-})
+  {
+    ...common,
+    entry: {
+      'node/index': 'src/node/index.ts',
+    },
+    clean: false,
+    fixedExtension: false,
+    platform: 'node',
+  },
+])
