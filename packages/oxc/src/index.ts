f3613d51329390d4ec500718af5aee584de73e16
yuyinws <lyc657508366@gmail.com>
2026-07-18 23:17:54 +0800

    refactor(oxc): delegate standalone CLI to devframe

--- packages/oxc/src/index.ts
+++ packages/oxc/src/index.ts
@@ -1,19 +1,13 @@
 import { argv } from 'node:process'
-import cac from 'cac'
-import { createDevServer } from 'devframe/adapters/dev'
+import { createCac } from 'devframe/adapters/cac'
 import { version } from '../package.json'
 import { oxcDevframe, OXC_DEVTOOLS_BASE } from './node/devframe'
 
-const cli = cac('oxc-devtools')
-
-cli.command('', 'Start Oxc DevTools').action(async () => {
-  await createDevServer(oxcDevframe, {
-    onReady: ({ origin }) => {
-      console.log(`Oxc Inspector UI is running on ${origin}${OXC_DEVTOOLS_BASE}`)
-    },
-  })
+const cli = createCac(oxcDevframe, {
+  onReady: ({ origin }) => {
+    console.log(`Oxc Devtools UI is running on ${origin}${OXC_DEVTOOLS_BASE}`)
+  },
 })
 
-cli.version(version)
-cli.help()
-cli.parse(argv)
+cli.cli.version(version)
+await cli.parse(argv)
