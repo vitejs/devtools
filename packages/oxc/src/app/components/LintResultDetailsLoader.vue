5edee0b7b6ada18d7a6d3cafc9d95dddcb96c013
yuyinws <lyc657508366@gmail.com>
2026-07-18 22:17:06 +0800

    feat(oxc): add lint result layout switcher

--- packages/oxc/src/app/components/LintResultDetailsLoader.vue
+++ packages/oxc/src/app/components/LintResultDetailsLoader.vue
@@ -33,6 +33,7 @@ const totalIssues = computed(() => {
 
 const showSummary = computed(() => !!lintResult.value?.meta.summary)
 const search = ref('')
+const singleColumn = ref(false)
 const debouncedSearch = ref('')
 const debouncedUpdateSearch = useDebounceFn((value: string) => {
   debouncedSearch.value = value
@@ -71,7 +72,11 @@ const showFiles = computed(() => filteredFiles.value.length > 0)
     />
 
     <VisualLoading v-if="isLoading" text="Loading lint result..." />
-    <div v-else class="h-full of-auto flex flex-col gap-4 p6 pr-10">
+    <div
+      v-else
+      class="h-full of-auto flex flex-col gap-4 p6 pr-10"
+      style="scrollbar-gutter: stable"
+    >
       <SummaryCard
         v-if="showSummary && lintResult?.meta.summary"
         :summary="lintResult.meta.summary"
@@ -81,7 +86,29 @@ const showFiles = computed(() => filteredFiles.value.length > 0)
         :timestamp="lintResult.meta.timestamp"
       />
 
-      <Search v-model="search" />
+      <div class="flex items-center gap-2">
+        <Search v-model="search" class="flex-1" />
+        <div
+          class="hidden lg:flex flex-none flex-row justify-around w20 h8 border border-base rounded-8 of-hidden"
+        >
+          <button
+            title="Show one column"
+            class="flex-1 op50 flex items-center justify-center hover:bg-active hover:text-base hover:op100!"
+            :class="{ 'bg-active text-base op100!': singleColumn }"
+            @click="singleColumn = true"
+          >
+            <span class="i-ph-rows-duotone text-sm" />
+          </button>
+          <button
+            title="Show two columns"
+            class="flex-1 op50 flex items-center justify-center hover:bg-active hover:text-base hover:op100!"
+            :class="{ 'bg-active text-base op100!': !singleColumn }"
+            @click="singleColumn = false"
+          >
+            <span class="i-ph-columns-duotone text-sm" />
+          </button>
+        </div>
+      </div>
 
       <VisualEmptyState
         v-if="showEmpty"
@@ -90,7 +117,15 @@ const showFiles = computed(() => filteredFiles.value.length > 0)
       />
 
       <template v-else>
-        <div v-if="showFiles" class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div
+          v-if="showFiles"
+          class="grid gap-4"
+          :class="
+            singleColumn || filteredFiles.length === 1
+              ? 'grid-cols-1'
+              : 'grid-cols-1 lg:grid-cols-2'
+          "
+        >
           <FileCard v-for="file in filteredFiles" :key="file.filename" :file="file" />
         </div>
 
