10f99581f2c5887570cb52142903410d7503169b
Anthony Fu <github@antfu.me>
2026-07-27 15:07:11 +0900

    fix(ui): remove custom web fonts usage to reduce package size

--- packages/vite/src/uno.config.ts
+++ packages/vite/src/uno.config.ts
@@ -1,17 +1,8 @@
-import { fileURLToPath } from 'node:url'
-import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
 import { presetDevToolsUI } from '@vitejs/devtools-ui/unocss'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
   presets: [
-    presetDevToolsUI({
-      webFonts: {
-        processors: createLocalFontProcessor({
-          fontAssetsDir: fileURLToPath(new URL('./public/fonts', import.meta.url)),
-          fontServeBaseUrl: '../fonts',
-        }),
-      },
-    }),
+    presetDevToolsUI(),
   ],
 })
