802691c83e8fc667fa9de540d53109a3460f0676
arlo <webfansplz@gmail.com>
2026-08-27 15:37:46 +0800

    fix(kit): restore rpc module augmentation

--- packages/kit/src/client/index.ts
+++ packages/kit/src/client/index.ts
@@ -7,12 +7,7 @@ export * from './remote'
 
 export {
   type DevframeClientRpcHost as DevToolsClientRpcHost,
-  type DevframeRpcClient as DevToolsRpcClient,
-  type DevframeRpcClientCall as DevToolsRpcClientCall,
-  type DevframeRpcClientCallEvent as DevToolsRpcClientCallEvent,
-  type DevframeRpcClientCallOptional as DevToolsRpcClientCallOptional,
   type DevframeRpcClientMode as DevToolsRpcClientMode,
-  type DevframeRpcClientOptions as DevToolsRpcClientOptions,
   type DevframeRpcContext as DevToolsRpcContext,
   type RpcStreamingClientHost,
   type StreamingSubscribeOptions,
